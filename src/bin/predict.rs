21a9501b97056f4e255c48096867a9b6e6d2cdb7
hferraud <ferraudhugo@gmail.com>
2024-09-26 18:26:29 +0200

    add the possibilty to predict without a model

--- src/bin/predict.rs
+++ src/bin/predict.rs
@@ -7,15 +7,17 @@ use linear_regression::linear_model::LinearModel;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    model_path: String,
     mileage: f64,
+    model_path: Option<String>,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
 
     let mut model = LinearModel::new();
-    model.load(&args.model_path)?;
+    if let Some(model_path) = args.model_path {
+        model.load(&model_path)?;
+    }
     println!(
         "Estimated price for {} km: {}",
         args.mileage,
